f690e2d8a467209776fd21035aa0c1260eac427f
onethejay <jhjang951@gmail.com>
2022-01-26 00:43:59 +0900

    Initial commit

---
+++ src/test/java/com/example/querydsldemo/QueryDslDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.querydsldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryDslDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
